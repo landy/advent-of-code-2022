12e66b2e89325f69a3a23b612ba76bb3c239ede1
Jiří Landsman <landsman@cngroup.dk>
2023-07-13 15:58:32 +0200

    day one ugly mutable solution

---
+++ src/DayOne/DayOne.fsx
@@ -0,0 +1,27 @@
+open System.Linq
+
+let loadInput day =
+    let filename = __SOURCE_DIRECTORY__ + $"/inputs/day{day}.txt"
+    printfn "Loading input from %s" filename
+
+    System.IO.File.ReadAllLines filename
+
+
+let input = loadInput 1
+
+let countElfs input =
+    let mutable elfNumber = 0
+    let mutable elfs = ResizeArray<int>()
+    let mutable elf = ResizeArray<int>()
+
+    for line in input do
+        if line = "" then
+            elfNumber <- elfNumber + 1
+            elfs.Add(elf.Sum())
+            elf <- ResizeArray<int>()
+        else
+            elf.Add(int line)
+
+    elfs |> Array.ofSeq
+
+input |> countElfs |> Array.length |> printfn "Sum of all elfs: %i"
